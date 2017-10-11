04f7c62d809b39b470b29fe2dc978e33c85b7f9b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 23:28:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,6 @@
 def turn_count(board)
   counter = 0
-  board.each {|spot|
-    if(spot == "X" || spot == "O")
-      counter +=1
-    end
-  }
+  board.each { |spot| if(spot == "X" || spot == "O") counter +=1 end }
   counter
   end
 end
